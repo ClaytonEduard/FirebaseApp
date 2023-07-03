0f6f76f42be3aad1dbdaaa764e6c4c2ad32e4f9f
ClaytonEduard <clayton_eduard@hotmail.com>
2023-07-03 12:16:15 -0300

    Recuperar dados do Firebase

--- app/src/main/java/com/claytoneduard/firebaseapp/MainActivity.java
+++ app/src/main/java/com/claytoneduard/firebaseapp/MainActivity.java
@@ -1,11 +1,16 @@
 package com.claytoneduard.firebaseapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,9 +27,11 @@ public class MainActivity extends AppCompatActivity {
         //reference.child("pontos").setValue("200");
 
         // pega a referencia do nó usuarios no banco
-        DatabaseReference usuarios = reference.child("usuarios");
+        DatabaseReference usuarios = reference.child("usuarios").child("001");
+        DatabaseReference produtos = reference.child("produtos");
 
-        // criando um ususario
+        // salvar dados
+       /*   // criando um ususario
         Usuario usuario = new Usuario();
         usuario.setNome("Nathalia");
         usuario.setSobrenome("Prateado");
@@ -41,6 +48,22 @@ public class MainActivity extends AppCompatActivity {
         produto.setPreço(7.999);
 
         produtos.child("001").setValue(produto);
+
+      */
+
+        //recuperar dados
+        usuarios.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.i("FIREBASE", snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
     }
 }
